b88c7360ac0bee986ce4d46b8ae42c692f9a183f
SehyeongLee <shseoul14@gmail.com>
2020-07-12 20:31:23 +0900

    Add lombok and lombok test code

--- src/main/java/com/sehyeong/springboot/web/HelloController.java
+++ src/main/java/com/sehyeong/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.sehyeong.springboot.web;
 
+import com.sehyeong.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,4 +14,9 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
+    }
 }
